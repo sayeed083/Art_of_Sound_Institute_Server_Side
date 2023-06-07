12d280c3af7b2a19ea8a96ae8fe2721c6f395fde
sayeed083 <sayeedislam407@gmailcom>
2023-06-07 23:17:15 +0600

    Added Post Method And Posted Users

--- index.js
+++ index.js
@@ -11,9 +11,89 @@ app.use(cors())
 app.use(express.json())
 
 
-
-
 //MongoDB Starts
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5nuw1j2.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        //Database and Collections Here
+        const userCollection = client.db("summerCamp").collection("user");
+
+        // All CRUD Operation Here
+
+        //User Side
+
+        // app.get('/users', async (req, res) => {
+        //     const result = await userCollection.find().toArray()
+        //     res.send(result)
+        // });
+
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existUser = await userCollection.findOne(query)
+            console.log("Already Here:", existUser);
+            if (existUser) {
+                return res.send({ message: 'User Already Exists' })
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //MongoDB Ends
 
 
